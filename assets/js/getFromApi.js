e32a2f376e61d3545ff3965c6799b2ac8cc9c01d
HernanS99 <hernan.silva2299@gmail.com>
2022-08-26 23:27:27 -0400

    solucion busqueda multiple

--- assets/js/getFromApi.js
+++ assets/js/getFromApi.js
@@ -1,97 +1,98 @@
-document.querySelector("#buscar").addEventListener("click",leerDatos)
+document.querySelector("#buscar").addEventListener("click", leerDatos)
 let response = []
 let myChart;
 let array = []
 let labelss = []
 let array2 = []
 
-async function leerDatos(){
-    let text = document.getElementById("textoBuscar").value;
+async function leerDatos() {
+  let text = document.getElementById("textoBuscar").value;
 
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${text}&units=metric&lang=es&appid=8ea115a8875190c8af50ed4a8da786b9`
-    
-    let config = {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${text}&units=metric&lang=es&appid=8ea115a8875190c8af50ed4a8da786b9`
 
-    }
-    response = await axios.get (url,config)
-    response = response.data
-   response = [...response.list]
-    
-   generarGrafico()
+  let config = {
+
+  }
+  response = await axios.get(url, config)
+  response = response.data
+  response = [...response.list]
+  
+  generarGrafico()
+  
 }
 
-function generarGrafico(){
+function generarGrafico() {
 
   array = response.map(Element => Element.main.temp_max)
   array2 = response.map(Element => Element.main.temp_min)
 
 
-  
+
   labelss = response.map(Element => Element.dt_txt)
   console.log(array)
   console.log(array2)
 
-    if(myChart)
-    {
-        myChart.destroy;
-    }
+  
 
-    const labels = [...labelss];
+  const labels = [...labelss];
 
-      const data = {
-        labels: labels,
-        datasets: [
-          {
-          label: 'Temperatura Maxima',
-          data: [...array],
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          borderCapStyle: 'butt',
-          borderDash: [5, 5],
-          animations: {
-            tension: {
-              duration: 1000,
-              easing: 'linear',
-              from: 1,
-              to: 0,
-              loop: true
-            }
-          },
-        },
-        {
-          label: 'Temperatura Minima',
-          data: [...array2],
-          backgroundColor: 'rgb(30,144,255)',
-          borderColor: 'rgb(30,144,255)',
-        }
-      ]
-      };
-      
-      const config = {
-        type: 'line',
-        data: data,
-        options: {
-            scales: {
-              y: {
-                  beginAtZero: true,
-                  ticks: {                    
-                      callback: function(value) {
-                          return value + '°'
-                      }
-                  }
-              },
-              x:{
-                  ticks: {
-                    maxRotation: 90,
-                    minRotation: 90
-                }
-              },
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Temperatura Maxima',
+        data: [...array],
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        borderCapStyle: 'butt',
+        borderDash: [5, 5],
+        animations: {
+          tension: {
+            duration: 1000,
+            easing: 'linear',
+            from: 1,
+            to: 0,
+            loop: true
           }
-        }
-      };
-    
-      myChart = new Chart(document.getElementById('myChart'),config);
-    
+        },
+      },
+      {
+        label: 'Temperatura Minima',
+        data: [...array2],
+        backgroundColor: 'rgb(30,144,255)',
+        borderColor: 'rgb(30,144,255)',
+      }
+    ]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            callback: function (value) {
+              return value + '°'
+            }
+          }
+        },
+        x: {
+          ticks: {
+            maxRotation: 90,
+            minRotation: 90
+          }
+        },
+      }
+    }
+  };
+  if(myChart != undefined){
+    myChart.destroy();
+  }
+  myChart = new Chart(document.getElementById('myChart'), config);
+  
+  
 }
 
 
